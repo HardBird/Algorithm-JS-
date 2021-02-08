fbe9bb8254d0ecb045dc6e96198f8e0d517679bd
HardBird <67629949+HardBird@users.noreply.github.com>
2021-02-08 17:20:17 +0900

    .

--- Programmers_KAKAO_Q1.js
+++ Programmers_KAKAO_Q1.js
@@ -10,12 +10,12 @@ function solution(board,moves){
         let cnt = 0;
         while(true){
             if (board[cnt][move-1] !== 0){
-                // 바구니에 인형이 있는데 잡은 인형과 같으면 터트린다
+                // 바구니에 인형이 있는데 잡은 인형과 같으면 터트린다.
                 if (pocket.length !== 0 && pocket[pocket.length-1] === board[cnt][move-1]){
                     pocket.pop();
                     result += 2;
                 }else{
-                // 그렇지 않다면 잡아서 옮긴다
+                // 그렇지 않다면 잡아서 옮긴다.
                     pocket.push(board[cnt][move-1]);
                 }
                 board[cnt][move-1] = 0;
